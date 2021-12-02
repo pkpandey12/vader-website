9b7ba618814c28b68f95bbfb05e440505b2717b1
pkpandey12 <praneetkpandey@gmail.com>
2021-12-02 10:41:01 +0800

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import * as Scroll from 'react-scroll';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+
+const App = () => {
+  return (
+    <div>
+      Hello world
+    </div>
+  )
+}
+export default App;
