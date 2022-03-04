0d0044461b02394d7b2c2d2a0bab0fc157df1767
pkpandey12 <praneetkpandey@gmail.com>
2022-03-04 18:33:13 +0800

    fix v12

--- src/components/ElevatorDoors.tsx
+++ src/components/ElevatorDoors.tsx
@@ -9,7 +9,7 @@ const ElevatorDoors = (props?:any) => {
   const [doorsOpen, setDoorsOpen] = useState(false);
 
   return(
-    <div id="elevator-container" style={{opacity: doorsOpen? '100%' : '0%'}}>
+    <div id="elevator-container">
     
       <Fade left when={!doorsOpen}>
       <div id="left-door">
